83c06af905cd188a86bc3fe7a0c5dc8c4c0e2e84
qaz10 <dfsh20519@gmail.com>
2019-05-23 21:36:50 +0800

    5/23

--- app/src/main/java/com/example/ptcare_cmu/DataBleMonitor.java
+++ app/src/main/java/com/example/ptcare_cmu/DataBleMonitor.java
@@ -1,5 +1,6 @@
 package com.example.ptcare_cmu;
 
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
@@ -12,6 +13,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -24,58 +26,55 @@ import com.mbientlab.metawear.MetaWearBoard;
 import java.util.UUID;
 
 public class DataBleMonitor extends AppCompatActivity {
-    private BluetoothAdapter mBluetoothAdapter;
-    private LocationManager status;
+    private BluetoothAdapter bluetoothAdapter;
+
     //
     public static final int REQUEST_START_BLE_SCAN = 1;
-    private static final int REQUEST_ENABLE_BT = 1;
-    //
-    private final static UUID[] mwServiceUuids = new UUID[] {
-            MetaWearBoard.METAWEAR_GATT_SERVICE,
-            MetaWearBoard.METABOOT_SERVICE
-    };
+    private static final int REQUEST_ENABLE_BT = 2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blemain);
 
-        //fab 是新增藍芽的按鈕
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(view -> startActivityForResult(new Intent(DataBleMonitor.this, ScannerActivity.class), REQUEST_START_BLE_SCAN));
-
-        //
-        final BluetoothManager bluetoothManager =
-                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
-        mBluetoothAdapter = bluetoothManager.getAdapter();
-        BluetoothLeScanner test = mBluetoothAdapter.getBluetoothLeScanner();
+        // 確認是否有開啟藍芽功能
+        BluetoothManager bluetoothManager = (BluetoothManager) this.getSystemService(Context.BLUETOOTH_SERVICE);
+        bluetoothAdapter = bluetoothManager.getAdapter();
         // Checks if Bluetooth is supported on the device.
-        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
-//            Toast.makeText(this, R.string.error_bluetooth_not_supported, Toast.LENGTH_SHORT).show();
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        if (bluetoothAdapter == null) {
+            Toast.makeText(this, "Device doesn't Support Bluetooth", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+        else {
+            if (!bluetoothAdapter.isEnabled()) {
+                Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+            }
         }
 
-        status = (LocationManager) (this.getSystemService(Context.LOCATION_SERVICE));
-        if(!status.isProviderEnabled(LocationManager.GPS_PROVIDER)&& !status.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
-            Toast.makeText(DataBleMonitor.this, "請開啟定位", Toast.LENGTH_LONG).show();
-            new AlertDialog.Builder(DataBleMonitor.this)
-                    .setTitle("Error")
-                    .setMessage("需要開啟定位")
+        // 確認是否有開啟定位功能
+        LocationManager locationStatus = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        boolean gps_enabled = locationStatus.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        boolean network_enabled = locationStatus.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        if (!gps_enabled && !network_enabled) {
+            new AlertDialog.Builder(this)
+                    .setTitle("提示訊息")
+                    .setMessage("需要開啟定位功能")
                     .setPositiveButton("OK", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
-                            startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)); //開啟設定頁面
+                            startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
                         }
                     })
-                    .setNegativeButton("NO", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            finish();
-                        }
-                    })
+                    .setNegativeButton("Cancel", (dialog, which) -> finish())
+                    .create()
                     .show();
         }
+
+        // fab 是新增藍芽的按鈕
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(view -> startActivityForResult(new Intent(this, ScannerActivity.class), REQUEST_START_BLE_SCAN));
 
     }
 
@@ -84,11 +83,20 @@ public class DataBleMonitor extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         switch (requestCode) {
             case REQUEST_START_BLE_SCAN:
-                // 获取查找到的蓝牙设备
-                BluetoothDevice selectedDevice= data.getParcelableExtra(ScannerActivity.EXTRA_DEVICE);
-                if (selectedDevice != null) {
-                    ((MainActivityFragment) getSupportFragmentManager().findFragmentById(R.id.main_activity_content)).addNewDevice(selectedDevice);
+                if (resultCode == Activity.RESULT_OK) {
+                    // 获取查找到的蓝牙设备
+                    BluetoothDevice selectedDevice = data.getParcelableExtra(ScannerActivity.EXTRA_DEVICE);
+                    if (selectedDevice != null) {
+                        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.main_activity_content);
+                        ((MainActivityFragment) fragment).addNewDevice(selectedDevice);
+                    }
                 }
+                break;
+            case REQUEST_ENABLE_BT:
+                if (resultCode == Activity.RESULT_OK)
+                    Toast.makeText(this,"已開啟藍芽功能",Toast.LENGTH_SHORT).show();
+                if (resultCode == Activity.RESULT_CANCELED)
+                    Toast.makeText(this,"並未開啟藍芽功能",Toast.LENGTH_SHORT).show();
                 break;
         }
     }
